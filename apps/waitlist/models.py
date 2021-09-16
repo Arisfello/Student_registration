14a7ea6a3845a20d5822c61e80188f4d9937fc7f
arisafello <felomwansa@mail.com>
2021-09-16 14:08:18 +0300

    second commit

--- apps/waitlist/models.py
+++ apps/waitlist/models.py
@@ -8,10 +8,12 @@ class WaitlistEntry(Timestamps, models.Model):
     email = models.EmailField(verbose_name='email address',
                               max_length=255,
                               unique=True, )
+    level = models.IntegerField(verbose_name='Class Level', default=1)
     notes = models.TextField()
 
-
     class Meta:
-        verbose_name_plural ='waitlist entries'
+        verbose_name_plural = 'waitlist entries'
 
+    def full_name(self):
+        return f'{self.first_name} {self.last_name}'
 
